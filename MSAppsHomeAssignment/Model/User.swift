e63907e1a5f51b693ba0cc87f3078e750ea6af2f
David Katsman <kadamc90@gmail.com>
2024-09-06 11:15:00 +0300

    Preparing Core Data

---
+++ MSAppsHomeAssignment/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  MSAppsHomeAssignment
+//
+//  Created by David Katsman on 06/09/2024.
+//
+
+import Foundation
---
+++ MSAppsHomeAssignment/Model/UsersManager.swift
@@ -0,0 +1,8 @@
+//
+//  UsersManager.swift
+//  MSAppsHomeAssignment
+//
+//  Created by David Katsman on 06/09/2024.
+//
+
+import Foundation
